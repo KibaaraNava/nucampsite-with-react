9dba63e6ed5aee29ac4837b40ec2d1baece216cb
Ngure <ngurekibaara01@gmail.com>
2024-12-08 17:05:34 +0300

    Partners Component and List

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -19,6 +20,7 @@ function App() {
                 <Route path='directory/:campsiteId'
                     element={<CampsiteDetailPage />}
                 /> 
+                <Route path="about" element={<AboutPage />} />
             </Routes>
             <Footer />
         </div>
