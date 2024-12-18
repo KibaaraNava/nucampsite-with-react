51ed4c69acb4f678c96d665f0dcf45e5c8476eba
Ngure <ngurekibaara01@gmail.com>
2024-12-18 13:35:37 +0300

    Redux: useDispatch and Middleware

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { addComment } from "./commentsSlice";
 
 const CommentForm = ({campsiteId}) => {
     const [ modalOpen, setModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
 
     const handleSubmit = (values) => {
         const comment = {
@@ -12,12 +16,14 @@ const CommentForm = ({campsiteId}) => {
             campsiteId: parseInt(campsiteId),
             rating: parseInt(values.rating),
             text: values.commentText,
-            author: values.author
-        }
+            author: values.author,
+            date: new Date(Date.now()).toISOString()
+        };
 
         console.log(comment);
+        dispatch(addComment(comment));
         setModalOpen(false);
-    }
+    };
 
     return (
         <>
